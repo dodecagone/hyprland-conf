1daac80adfb2fffb966d46e68581ceecd6db673e
ddcgn <mars92120@gmail.com>
2025-03-31 13:58:06 +0200

    Add files via upload

---
+++ hyprland_config/setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cp $HOME/.config/hypr/hyprland.conf $HOME/.config/hypr/hyprland.conf 
+cp ./.config/hypr/* $HOME/.config/hypr/
+
+cp $HOME/.config/hypr/hyprlock.conf $HOME/.config/hypr/hyprlock.conf
